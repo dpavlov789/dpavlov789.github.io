dc3d3a2a0cd7b48d7d210f7339f7e7d15dabacc5
DPavlov <dpavlov789@gmail.com>
2026-01-21 00:48:05 +0300

    wp test del

--- bb/wp-content/themes/bb/footer.php
+++
@@ -1,8 +0,0 @@
-    <footer>
-        <div class="container">
-            <p>&copy; 2025 Basketball Wetten Guide. Alle Rechte vorbehalten. | Spielen Sie verantwortungsvoll. | 18+</p>
-        </div>
-    </footer>
-    <?php wp_footer(); ?>
-</body>
-</html>
--- bb/wp-content/themes/bb/functions.php
+++
@@ -1,6 +0,0 @@
-<?php 
-add_action( 'wp_enqueue_scripts', 'bb_scripts' );
-function bb_scripts() {
-	wp_enqueue_style( 'style', get_template_directory_uri() . '/assets/css/style.css' );
-}
-?>
